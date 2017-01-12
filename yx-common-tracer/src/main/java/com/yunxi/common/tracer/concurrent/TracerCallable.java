e36749b5ecf85d38095362438eb264b12905c309
leukony <leukony@yeah.net>
2017-01-12 16:40:19 +0800

    tracer多线程

--- yx-common-tracer/src/main/java/com/yunxi/common/tracer/concurrent/TracerCallable.java
+++ yx-common-tracer/src/main/java/com/yunxi/common/tracer/concurrent/TracerCallable.java
@@ -8,6 +8,7 @@ import com.yunxi.common.tracer.util.TracerUtils;
 
 /**
  * Tracer多线程支持：Callable实现
+ * 
  * @author <a href="mailto:leukony@yeah.net">leukony</a>
  * @version $Id: TracerCallable.java, v 0.1 2017年1月11日 下午7:02:03 leukony Exp $
  */
--- yx-common-tracer/src/main/java/com/yunxi/common/tracer/concurrent/TracerRunnable.java
+++ yx-common-tracer/src/main/java/com/yunxi/common/tracer/concurrent/TracerRunnable.java
@@ -6,6 +6,7 @@ import com.yunxi.common.tracer.util.TracerUtils;
 
 /**
  * Tracer多线程支持：Runnable实现 
+ * 
  * @author <a href="mailto:leukony@yeah.net">leukony</a>
  * @version $Id: TracerRunnable.java, v 0.1 2017年1月11日 下午7:05:01 leukony Exp $
  */
