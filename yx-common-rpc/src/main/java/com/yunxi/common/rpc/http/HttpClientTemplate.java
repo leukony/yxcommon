43efe1123435e79a5dd1b018c2ab6d27bf10f073
leukony <leukony@yeah.net>
2017-01-12 09:35:52 +0800

    HttpClient

--- yx-common-rpc/src/main/java/com/yunxi/common/rpc/http/HttpClientTemplate.java
+++ yx-common-rpc/src/main/java/com/yunxi/common/rpc/http/HttpClientTemplate.java
@@ -1,6 +1,16 @@
 package com.yunxi.common.rpc.http;
 
 /**
+ * HttpClient模板
+ * 
+ * <p>
+ * 基于Apache Common HttpClient:
+ * <ul>
+ * <li>设置默认超时参数</li>
+ * <li>执行Http请求</li>
+ * <li>断开连接</li>
+ * </ul>
+ * <p>
  * 
  * @author <a href="mailto:leukony@yeah.net">leukony</a>
  * @version $Id: HttpClientTemplate.java, v 0.1 2017年1月12日 上午9:25:11 leukony Exp $
